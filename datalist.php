a4469e15336b98a8ed20723b1bde6a2887f78b62
Mehedi Hasan Himel <mehedihasanhimel260@gmail.com>
2022-06-21 15:30:07 +0600

    OPP design

---
+++ datalist.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Table 03</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">OOP CRUD</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="datalist.php">Display</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Sign Up</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <section class="ftco-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h4 class="text-center mb-4">Create Your Domain Name</h4>
+                    <div class="table-wrap">
+                        <table class="table">
+                            <thead class="table-dark">
+                                <tr class="text-center">
+                                    <th>TLD</th>
+                                    <th>Duration</th>
+                                    <th>Registration</th>
+                                    <th>Renewal</th>
+                                    <th>Transfer</th>
+                                    <th>Register</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="text-center">
+                                    <th scope="row" class="scope">.com</th>
+                                    <td>1 Year</td>
+                                    <td>$70.00</td>
+                                    <td>$5.00</td>
+                                    <td>$5.00</td>
+                                    <td>
+                                        <a href="#" class="btn btn-primary">Edite</a>
+                                        <a href="#" class="btn btn-primary ">Delete</a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
